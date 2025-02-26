cab01556801cfc601ef643a07f7598c057e85743
matthlmes <matt250402@gmail.com>
2025-02-26 13:50:02 +0000

    added server file and changed to .ejs

---
+++ server.js
@@ -0,0 +1,121 @@
+// BCRYPT SETUP
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
+// CONNECT TO MONGO
+const MongoClient = require('mongodb-legacy').MongoClient;
+const url = 'mongodb://127.0.0.1:27017';
+const client = new MongoClient(url);
+const dbname = 'rguwork';
+
+// LOAD NPM PACKAGES
+let express = require('express');
+let session = require('express-session');
+let bodyParser = require('body-parser');
+const {Console, profile} = require('console');
+const app = express();
+
+app.use(session({secret: 'example'}));
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.set('view engine', 'ejs');
+
+// CONNECT TO DB
+let db;
+connectDB();
+async function connectDB(){
+    await client.connect();
+    console.log('Connected Successfully to Server');
+    db = client.db(dbname);
+    app.listen(8080);
+    console.log('Connected to Port: 8080');
+};
+
+// RENDER PAGES
+
+
+// INDEX PAGE
+app.get('/', function(req, res){
+    res.render('pages/index');
+});
+
+// HOME PAGE
+app.get('/home', function(req, res){
+    if(!req.session.loggedin){res.redirect('/');return;}
+        res.render('pages/home');
+   
+});
+
+
+
+// SIGN-UP
+app.post('/signup', async function(req, res){
+
+    bcrypt.genSalt(saltRounds, function(err, salt){
+        if(err) throw err;
+        bcrypt.hash(req.body.psw, salt, function(err, hash){
+            if(err) throw err;
+            let datatostore = {
+                "email": req.body.email,
+                "login": {"username": req.body.uname, "password": hash},
+            }
+
+            let uname = req.body.uname;
+            db.collection('users').findOne({"login.username":uname}, function(err, result){
+                if(err) throw err;
+
+                if(!result){
+                    db.collection('users').insertOne(datatostore, function(err, result){
+                        if(err) throw err;
+                        console.log("User Created");
+                        res.redirect('/');
+                    });
+                } else {
+                    console.log("User Already Exists");
+                    res.redirect('/users');
+                }
+            });
+        });
+    });
+});
+
+
+// LOGIN
+app.post('/login', async function(req, res){
+    let username = req.body.username;
+    let password = req.body.password;
+
+    db.collection('users').findOne({"login.username":username}, function(err, result){
+        if (err) throw err;
+        
+        //IF NO USER REDIRECT TO INDEX
+        if(!result){res.redirect('/');
+            console.log('No User Found')
+        return
+        }
+
+        bcrypt.compare(password, result.login.password, function(err, result) {
+        // result == true
+        console.log(result);
+        //CHECKS PASSWORD AGAINST USER
+            if(result == true){
+                console.log("true")
+                console.log(result);
+                req.session.loggedin = true; 
+                req.session.currentuser = username;
+                res.redirect('/dashboard');
+            } else {
+                res.redirect('/')
+            }
+        });
+    });
+});
+
+//LOGOUT
+app.get('/logout', function(req, res){
+    req.session.loggedin = false;
+    req.session.destroy();
+    res.redirect('/');
+});
