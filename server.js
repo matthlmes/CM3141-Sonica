c459a4f8fdc1fee0d96b93f22eaf18d2e00da5c2
matthlmes <matt250402@gmail.com>
2025-02-26 14:30:51 +0000

    added comment to explain a bit of code

--- server.js
+++ server.js
@@ -45,7 +45,7 @@ app.get('/', function(req, res){
 
 // HOME PAGE
 app.get('/home', function(req, res){
-    if(!req.session.loggedin){res.redirect('/');return;}
+    if(!req.session.loggedin){res.redirect('/');return;}    //Checks user is logged in, if not send them back to the log in page
         res.render('pages/home');
    
 });
