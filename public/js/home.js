7d016ac789b80434a70e0de74645c93b177e88b2
matthlmes <matt250402@gmail.com>
2025-02-26 14:39:15 +0000

    added time of day to greeting

---
+++ public/js/home.js
@@ -0,0 +1,19 @@
+
+//Get time and apply general term to it
+function getTimeOfDay() {
+    var now = new Date();
+    var hour = now.getHours();
+    var timeOfDay;
+
+    if (hour < 12) {
+        timeOfDay = "Morning";
+    } else if (hour < 18) {
+        timeOfDay = "Afternoon";
+    } else {
+        timeOfDay = "Evening";
+    }
+
+    return timeOfDay;
+}
+
+document.getElementById("greeting").innerText = "Good " + getTimeOfDay() + " , ";
