0168b1047c19d7d9cd779cb8903cb908eeb61653
Craig-Leslie <95241379+Craig-Leslie@users.noreply.github.com>
2025-02-27 12:31:28 +0000

    updated signup page

--- public/js/startPage.js
+++ public/js/startPage.js
@@ -5,7 +5,20 @@ window.onload = function(){
             document.getElementById("loginForm").style.display = "none";  
         }
         else{
+            document.getElementById("signupForm").style.display = "none";  
             document.getElementById("loginForm").style.display = "flex";
+        }
+
+    }
+
+    document.getElementById("SignupBox").addEventListener("click", startSignup)
+    function startSignup(){
+        if(document.getElementById("signupForm").style.display == "flex"){
+            document.getElementById("signupForm").style.display = "none";  
+        }
+        else{
+            document.getElementById("loginForm").style.display = "none";  
+            document.getElementById("signupForm").style.display = "flex";
         }
 
     }
