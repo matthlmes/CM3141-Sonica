a1cdb677b5d4e121d0db5cbb2fdfbfd34490c586
matthlmes <matt250402@gmail.com>
2025-03-11 20:50:07 +0000

    added console.log

--- public/js/calendarWeek.js
+++ public/js/calendarWeek.js
@@ -9,6 +9,7 @@ document.addEventListener('DOMContentLoaded', function() {
       }
     });
     calendar.render();
+    console.log(eventArray[0].back);
     calendar.addEvent([ eventArray[0].back ] )
 
     /* document.getElementById("addEvent").addEventListener("click", function(){
