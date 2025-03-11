106a1ada2d46499977c839402768481c306ee26f
Matthew <75327868+matthlmes@users.noreply.github.com>
2025-03-11 10:06:23 +0000

    calendar day week in home.ejs

---
+++ public/js/calendarDay.js
@@ -0,0 +1,12 @@
+import { Calendar } from '@fullcalendar/core'
+import dayGridPlugin from '@fullcalendar/daygrid'
+
+const calendar = new Calendar(calendarEl, {
+  plugins: [dayGridPlugin],
+  initialView: 'dayGridWeek',
+  headerToolbar: {
+    left: 'prev,next',
+    center: 'title',
+    right: 'dayGridWeek,dayGridDay' // user can switch between the two
+  }
+})
