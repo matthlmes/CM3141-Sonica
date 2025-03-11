2490f8104e10ac91a37967cffc3de87d0ab364b6
Matthew <75327868+matthlmes@users.noreply.github.com>
2025-03-11 10:38:10 +0000

    added big calendar to calendar page

--- public/js/calendarWeek.js
+++ public/js/calendarWeek.js
@@ -1,12 +1,11 @@
-
 document.addEventListener('DOMContentLoaded', function() {
     var calendarEl = document.getElementById('calendar');
     var calendar = new FullCalendar.Calendar(calendarEl, {
-      initialView: 'dayGridMonth',
+      initialView: 'dayGridWeek',
       headerToolbar: {
         left: 'prev,next',
         center: 'title',
-        right: 'dayGridMonth,dayGridWeek,dayGridDay' // user can switch between the two
+        right: 'dayGridWeek,dayGridDay' // user can switch between the two
       }
     });
     calendar.render();
