9562f1c7d21509c5a06c5b1ea9c1e0cbd54b99d7
Matthew <75327868+matthlmes@users.noreply.github.com>
2025-03-11 10:35:39 +0000

    added change to day or week

--- public/js/calendar.js
+++ public/js/calendar.js
@@ -2,7 +2,12 @@
 document.addEventListener('DOMContentLoaded', function() {
     var calendarEl = document.getElementById('calendar');
     var calendar = new FullCalendar.Calendar(calendarEl, {
-      initialView: 'dayGridWeek'
+      initialView: 'dayGridWeek',
+      headerToolbar: {
+        left: 'prev,next',
+        center: 'title',
+        right: 'dayGridWeek,dayGridDay' // user can switch between the two
+      }
     });
     calendar.render();
   });
